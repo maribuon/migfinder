cf27610f3985fb40424d2052d260e30b09926c68
Ales-ibt <ales@ebi.ac.uk>
2022-08-19 09:24:34 +0100

    Update migfinder.py

--- migfinder/migfinder.py
+++ migfinder/migfinder.py
@@ -533,6 +533,8 @@ def posproc2(prefix, output_directory, k_orf, d_CDS_attC = 500, dist_threshold=4
 		# remove always the CDS.
 		m = 0
 		while m < M:
+			print(data[m][7])
+			print(type(data[m][7))
 			if int(data[m][7]) < int(-50):
 				if data[m][1] == "CDS":
 					del data[m]
