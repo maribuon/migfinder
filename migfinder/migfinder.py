9040e628ffb2f07e4dd3d539dde72e031a9ee35d
Ales-ibt <ales@ebi.ac.uk>
2022-08-19 09:20:05 +0100

    Update migfinder.py

--- migfinder/migfinder.py
+++ migfinder/migfinder.py
@@ -533,7 +533,7 @@ def posproc2(prefix, output_directory, k_orf, d_CDS_attC = 500, dist_threshold=4
 		# remove always the CDS.
 		m = 0
 		while m < M:
-			if data[m][7] < int(-50):
+			if int(data[m][7]) < int(-50):
 				if data[m][1] == "CDS":
 					del data[m]
 					remove_overlap += 1
